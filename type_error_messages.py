c1808931661af258f09a169f39ec628134866a1d
alessiovignoli <alessio.vignoli3@studio.unibo.it>
2023-07-04 15:22:08 +0200

    added the possibility to send a custom message to be printed

--- type_error_messages.py
+++ type_error_messages.py
@@ -18,13 +18,17 @@ class TypeErrorMessage(ABC):
 
 class StrTypeErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, str):
-            if self.no_print:
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Variable is not string.")
+            elif self.no_print:
                 raise TypeError("Variable is not string.")
             else:
                 print('Variable is not string,   given :', self.variable, '  type:', type(self.variable), "\n", file=stderr)
@@ -33,13 +37,17 @@ class StrTypeErr(TypeErrorMessage):
 
 class BytesTypeErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, bytes):
-            if self.no_print:
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Variable is not bytes.")
+            elif self.no_print:
                 raise TypeError("Variable is not bytes.")
             else:
                 print('Variable is not bytes,   given :', self.variable, '  type:', type(self.variable), "\n", file=stderr)
@@ -48,13 +56,17 @@ class BytesTypeErr(TypeErrorMessage):
 
 class BytesStrErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, str) and not isinstance(self.variable, bytes):
-            if self.no_print:
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Variable is nor string nor bytes.")
+            elif self.no_print:
                 raise TypeError("Variable is nor string nor bytes.")
             else:
                 print('Variable is nor string nor bytes,   given :', self.variable, '  type:', type(self.variable), "\n", file=stderr)
@@ -63,12 +75,16 @@ class BytesStrErr(TypeErrorMessage):
 
 class IntTypeErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, int):
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Variable is not int.")
             if self.no_print:
                 raise TypeError("Variable is not int.")
             else:        
@@ -78,12 +94,16 @@ class IntTypeErr(TypeErrorMessage):
 
 class ListTypeErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, list):
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Variable is not a list.")
             if self.no_print:
                 raise TypeError("Variable is not a list.")
             else:
@@ -94,12 +114,16 @@ class ListTypeErr(TypeErrorMessage):
 
 class FileTypeErr(TypeErrorMessage):
 
-    def __init__(self, variable, no_print=False) -> None:
+    def __init__(self, variable, no_print=False, custom_print=False) -> None:
         self.variable = variable
         self.no_print = no_print
+        self.custom_print = custom_print
 
     def Asses_Type(self):
         if not isinstance(self.variable, io.TextIOBase) and not isinstance(self.variable, io.BufferedIOBase):
+            if self.custom_print:
+                print(self.custom_print)
+                raise TypeError("Invalid file object.")
             if self.no_print:
                 raise TypeError("Invalid file object.")
             else:
